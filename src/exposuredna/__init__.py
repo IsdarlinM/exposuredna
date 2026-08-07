4dddb1475388e7a857251c81a44bbe387941e5c9
isdarlinm <82850219+IsdarlinM@users.noreply.github.com>
2026-08-06 22:15:04 -0400

    Export entity resolution v2 primitives

--- src/exposuredna/__init__.py
+++ src/exposuredna/__init__.py
@@ -1,1 +1,24 @@
-__version__ = "0.2.0"
+"""Exposure DNA organization security knowledge graph."""
+
+from .resolution import (
+    HumanResolutionDecision,
+    NegativeConstraint,
+    RelationshipType,
+    ResolutionCandidate,
+    ResolutionReview,
+    ResolutionSignal,
+    apply_human_review,
+    evaluate_resolution,
+)
+
+__all__ = [
+    "HumanResolutionDecision",
+    "NegativeConstraint",
+    "RelationshipType",
+    "ResolutionCandidate",
+    "ResolutionReview",
+    "ResolutionSignal",
+    "apply_human_review",
+    "evaluate_resolution",
+]
+__version__ = "0.3.0"
